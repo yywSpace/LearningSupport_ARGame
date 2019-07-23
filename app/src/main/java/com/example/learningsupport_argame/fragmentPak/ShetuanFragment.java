60eea4b6341265fbd4f93f6af7fc7af5b96cae3a
邝文卿 <1024611180@qq.com>
2019-07-23 14:57:09 +0800

    data:2019.7.23 afternoon 界面逻辑基本做完，填入数据即可。

--- app/src/main/java/com/example/learningsupport_argame/fragmentPak/ShetuanFragment.java
+++ app/src/main/java/com/example/learningsupport_argame/fragmentPak/ShetuanFragment.java
@@ -7,16 +7,43 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.learningsupport_argame.R;
+import com.example.learningsupport_argame.adapter.MsgAdapter;
+import com.example.learningsupport_argame.bean.Msg;
+import com.example.learningsupport_argame.util.MsgUtil;
+
+import java.util.List;
 
 public class ShetuanFragment extends Fragment {
+
+    private ListView listView;
+
+    private List<Msg> msgList;
+
+    private MsgAdapter adapter;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.shetuan_fragment_layout, container, false);
         return view;
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+
+
+        listView = getActivity().findViewById(R.id.listview);
+
+        msgList = com.example.learningsupport_argame.util.MagUtil_shetuan.getMsgList();
+
+        adapter = new MsgAdapter(msgList,getContext());
+
+        listView.setAdapter(adapter);
 
     }
 }
