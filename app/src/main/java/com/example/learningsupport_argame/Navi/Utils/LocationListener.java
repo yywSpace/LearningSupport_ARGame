2788631fd750257f02129f690860ed314931d749
Liu-shengnan <1531653056@qq.com>
2019-07-26 18:52:46 +0800

    MapNavigation

---
+++ app/src/main/java/com/example/learningsupport_argame/Navi/Utils/LocationListener.java
@@ -0,0 +1,99 @@
+package com.example.learningsupport_argame.Navi.Utils;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+
+public class LocationListener extends BDAbstractLocationListener {
+
+    LatLng mUser_latlng;
+    double latitude;
+    double longitude;
+    float radius;
+    String coorType;
+    int errorCode;
+    private MapView mMapView;
+    private BaiduMap mBaiduMap;
+    boolean isFirstLoc;
+    BDLocation userlocation;
+
+    public void getMap(com.baidu.mapapi.map.MapView mapView, com.baidu.mapapi.map.BaiduMap baiduMap, boolean isFirstLoc) {
+        mMapView = mapView;
+        mBaiduMap = baiduMap;
+        this.isFirstLoc = isFirstLoc;
+
+    }
+
+    @Override
+    public void onReceiveLocation(BDLocation location) {
+
+        userlocation=location;
+
+        //mapView 销毁后不在处理新接收的位置
+        if (location == null || mMapView == null) {
+            return;
+        }
+        MyLocationData locData = new MyLocationData.Builder()
+                .accuracy(location.getRadius())
+                // 此处设置开发者获取到的方向信息，顺时针0-360
+                .direction(location.getDirection()).latitude(location.getLatitude())
+                .longitude(location.getLongitude()).build();
+
+        mBaiduMap.setMyLocationData(locData);
+
+
+        if (isFirstLoc) {
+            isFirstLoc = false;
+            LatLng ll = new LatLng(location.getLatitude(),
+                    location.getLongitude());
+
+            // 设置缩放比例,更新地图状态
+            float f = mBaiduMap.getMaxZoomLevel();// 19.0
+            MapStatusUpdate u = MapStatusUpdateFactory.newLatLngZoom(ll, f - 2);
+            mBaiduMap.animateMapStatus(u);
+
+
+        }
+
+
+
+//        latitude = location.getLatitude();    //获取纬度信息
+//         longitude = location.getLongitude();    //获取经度信息
+//         radius = location.getRadius();    //获取定位精度，默认值为0.0f
+//
+//         coorType = location.getCoorType();
+//        //获取经纬度坐标类型，以LocationClientOption中设置过的坐标类型为准
+//
+//         errorCode = location.getLocType();
+//        //获取定位类型、定位错误返回码，具体信息可参照类参考中BDLocation类中的说明
+
+    }
+
+    public double getLatitude()
+    {
+        return latitude;
+    }
+    public double getLongitude()
+    {
+        return longitude;
+    }
+    public LatLng getUserLocation()
+    {
+
+        mUser_latlng = new LatLng(userlocation.getLatitude(), userlocation.getLongitude());
+        return mUser_latlng;
+    }
+    public LatLng getGCJ02NaviLocation()
+    {
+
+        LocationClient.getBDLocationInCoorType(userlocation,BDLocation.BDLOCATION_BD09LL_TO_GCJ02);
+        mUser_latlng = new LatLng(userlocation.getLatitude(), userlocation.getLongitude());
+        return mUser_latlng;
+    }
+}
