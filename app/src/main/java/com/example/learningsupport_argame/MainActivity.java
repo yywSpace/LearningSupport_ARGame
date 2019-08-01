a632f3cdcea061134d385749f42d5e3d369141c3
邝文卿 <1024611180@qq.com>
2019-08-01 15:34:53 +0800

    data:2019.8.1 测试配合项目工作，导入unity的module。

--- app/src/main/java/com/example/learningsupport_argame/MainActivity.java
+++ app/src/main/java/com/example/learningsupport_argame/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.learningsupport_argame;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn_start;
+    private Context con;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_start= findViewById(R.id.start);
+        con=this;
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(con, GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
